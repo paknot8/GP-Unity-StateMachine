f455b2eb356c56e06a3b2d4f7d1a8dfd66e4ddf7
Erick <19063989@student.hhs.nl>
2024-02-16 22:28:26 +0100

    Added jump function

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -6,68 +6,54 @@ using UnityEngine.UIElements;
 
 public class MovementScript : MonoBehaviour
 {
-    [SerializeField] private Rigidbody RigidBody;
-    public float baseSpeed = 10;
-    public float movementMultiplier = 2;
-    private bool isSprinting = false;
+    Rigidbody RigidBody;
+    public float baseSpeed = 3;
+    public float speedMultiplier = 2;
+    public float jumpForce = 7;
+    private bool jump = false;
+    private bool isGrounded;
+
+    // Bewegen van het object naar voren en achter en zuikant.
+    private Vector3 movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        RigidBody = GetComponent<Rigidbody> ();
+        RigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // --- Basic ---
-        // if(Input.GetKey(KeyCode.A))
-        // {
-        //     transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-        // } 
-        // else if (Input.GetKey(KeyCode.D))
-        // {
-        //     transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-        // }
-        // else if (Input.GetKey(KeyCode.W))
-        // {
-        //     transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
-        // }
-        // else if (Input.GetKey(KeyCode.S))
-        // {
-        //     transform.position += Vector3.back * moveSpeed * Time.deltaTime;
-        // }
-        // else if (Input.GetKey(KeyCode.Space))
-        // {
-        //     transform.position += Vector3.up * moveSpeed * Time.deltaTime;
-        // }
         Movement();
-        
-        
     }
 
     void Movement()
     {
-        // Zorgt voor een smooth movement voor de X and Z, and unity weet dat GetAxis de input is van WASD
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
-        if (Input.GetKey(KeyCode.LeftShift))
+        // Smooth movement of WASD and arrow keys
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        movement = new Vector3(x, 0, z);
+
+        if(Input.GetKey(KeyCode.LeftShift))
         {
-            isSprinting = true;
-        }
+            movement *= baseSpeed + speedMultiplier;
+        } 
         else
         {
-            isSprinting = false;
+            movement *= baseSpeed;
         }
-            // Bewegen van het object naar voren en achter en zuikant.
-            Vector3 movement = new Vector3(x, 0, z);
+        if(Input.GetKey(KeyCode.Space))
+        {
 
-            //movement = x * transform.right + z * transform.forward;
-
-            if(isSprinting == true){
-                movement *= movementMultiplier;
+            isGrounded = RigidBody.velocity.y == 0; // check if it hits the ground
+            if(isGrounded == true){
+                jump = true;
+                RigidBody.velocity = new Vector3 (RigidBody.velocity.x, jumpForce, RigidBody.velocity.y * Time.deltaTime);
             }
+        }
 
-            transform.Translate(movement * baseSpeed * Time.deltaTime);
-    }
+        transform.Translate(movement * baseSpeed * Time.deltaTime); 
+    } 
+    
 }
