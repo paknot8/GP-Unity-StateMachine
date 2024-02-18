3af80ca64f5568043904f7db2f16df56570b0eae
Erick <19063989@student.hhs.nl>
2024-02-18 14:15:42 +0100

    Character kan now Walk and turn

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
     Rigidbody RigidBodyPlayer;
-    public float baseSpeed = 3;
-    public float speedMultiplier = 2;
+    public float baseSpeed = 5;
+    public float speedMultiplier = 8;
     public float jumpForce = 6;
     public float gravity = -10;
     public float rotationSpeed = 720;
@@ -33,7 +33,7 @@ public class MovementScript : MonoBehaviour
         float verticalZ = Input.GetAxis("Vertical");
         movementDirection = new Vector3(horizontalX, 0, verticalZ); // input value for character move
         movementDirection.Normalize(); // ensure magnitude of 1
-        transform.Translate(movementDirection * baseSpeed * Time.deltaTime, Space.World); // Move to direction we want (real world time)
+        
 
         // check if character is moving
         if(movementDirection != Vector3.zero)
@@ -43,14 +43,15 @@ public class MovementScript : MonoBehaviour
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime); // do the movement
         }
 
-        // if(Input.GetKey(KeyCode.LeftShift))
-        // {
-        //     movementDirection *= baseSpeed + speedMultiplier;
-        // } 
-        // else
-        // {
-        //     movementDirection *= baseSpeed;
-        // }
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(movementDirection * (baseSpeed + speedMultiplier) * Time.deltaTime, Space.World);
+        } 
+        else
+        {
+            transform.Translate(movementDirection * baseSpeed * Time.deltaTime, Space.World); // Move to direction we want (real world time)
+        }
+        
         // if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
         // {
         //     RigidBodyPlayer.AddForce(transform.up * jumpForce, ForceMode.Impulse);
