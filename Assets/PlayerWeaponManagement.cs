8e88a6e898d1f99342bdae4a03f2d84964ec87ea
Erick <19063989@student.hhs.nl>
2024-03-06 11:09:38 +0100

    fixed some slash bugs

--- Assets/PlayerWeaponManagement.cs
+++ Assets/PlayerWeaponManagement.cs
@@ -3,24 +3,23 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public float slashSpeed = 100f; // Adjust the speed of the slash
-    private bool slashingUp = true;
+    private bool slashing = true;
 
     // Update is called once per frame
     void Update()
     {
-        // Check if the weapon is slashing up or down
-        float slashDirection = slashingUp ? 1f : -1f;
+        DoSlash();
+    }
 
-        // Calculate the rotation angle based on time and speed
-        float rotationAngle = slashDirection * slashSpeed * Time.deltaTime;
+    private void DoSlash()
+    {
+        float slashDirection = slashing ? 1f : -1f; // Check if the weapon is slashing
+        float rotationAngle = slashDirection * slashSpeed * Time.deltaTime; // Calculate the rotation angle based on time and speed
+        transform.Rotate(Vector3.up, rotationAngle); // Rotate the weapon around its local Y-axis
 
-        // Rotate the weapon around its local Y-axis
-        transform.Rotate(Vector3.up, rotationAngle);
-
-        // Change the slash direction when reaching a certain angle (e.g., 45 degrees)
-        if (Mathf.Abs(transform.localRotation.eulerAngles.y) > 45f)
-        {
-            slashingUp = !slashingUp;
+        if (Mathf.Abs(transform.localRotation.eulerAngles.y) > 45f)// Change the slash direction when reaching a certain angle
+        { 
+            slashing = !slashing;
         }
     }
 }
