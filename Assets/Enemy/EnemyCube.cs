255cfbaea421038f0b517a6ed9070203dd801bb8
Erick <19063989@student.hhs.nl>
2024-03-11 11:42:07 +0100

    enemy clean up

--- Assets/Enemy/EnemyCube.cs
+++ Assets/Enemy/EnemyCube.cs
@@ -2,23 +2,17 @@ using UnityEngine;
 
 public class EnemyCube : Enemy
 {
-    // No need for the 'new' keyword here
-
     protected override void Update()
     {
-        // Additional logic specific to EnemyCube can be added here
-
-        // Call the base class Update method to maintain common functionality
+        // Additional logic specific to EnemyCube
         base.Update();
     }
 
     protected override void OnTriggerEnter(Collider other)
     {
-        // Additional logic specific to EnemyCube can be added here
-
-        // Call the base class OnTriggerEnter method to maintain common functionality
+        // Additional logic specific to EnemyCube
         base.OnTriggerEnter(other);
     }
-
-    // Override or add more methods as needed for the EnemyCube class
+    
+    // Add or override more methods as needed for the EnemyCube class
 }
