b68bcee71e396dca734a91332953c4f01f374813
Erick <19063989@student.hhs.nl>
2024-03-13 11:57:56 +0100

    Update Player.cs

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -34,7 +34,7 @@ public class Player : MonoBehaviour
         public float groundCheckRange = 0.5f;
     #endregion
 
-    #region Object References to instance of Player States 
+    #region Object References to Player States 
         public PlayerBaseState playerState;
         public PlayerIdleState idleState = new();
         public PlayerWalkState walkState = new();
