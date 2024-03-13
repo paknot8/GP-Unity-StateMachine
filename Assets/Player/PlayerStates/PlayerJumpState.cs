4944449315067668740cbaeaadc6ab094eb5744c
Erick <19063989@student.hhs.nl>
2024-03-13 12:36:29 +0100

    Working version Final

--- Assets/Player/PlayerStates/PlayerJumpState.cs
+++ Assets/Player/PlayerStates/PlayerJumpState.cs
@@ -7,7 +7,7 @@ public class PlayerJumpState : PlayerBaseState
         if (player.IsGrounded()){
             player.jumpSound.Play();
             player.rigidBody.AddForce(Vector3.up * player.jumpForce, ForceMode.Impulse);
-            // Add forward force
+            // Add forward force, after jumping player moves a little bit forward
             player.rigidBody.AddForce(player.transform.forward * player.forwardJumpForce, ForceMode.Impulse);
             // Red - indicates has jumped
             player.GetComponent<MeshRenderer>().material.color = new Color32(250, 00, 45, 255);
@@ -19,8 +19,6 @@ public class PlayerJumpState : PlayerBaseState
     public override void UpdateState(Player player)
     {
         if (!player.IsGrounded())
-        {
             player.ChangeState(player.fallState);
-        }
     }
 }
